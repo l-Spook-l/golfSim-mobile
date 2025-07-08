1eccd9e6c6877cb33a386830e1542456bc118b61
l-Spook-l <uaspookua@gmail.com>
2025-07-08 20:08:28 +0300

    Initial commit with Android project

---
+++ OpenCV/build/intermediates/cxx/RelWithDebInfo/6m5m1vg1/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\mihai\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HS:\\Android projects\\GolfSimMobile\\OpenCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=24" ^
+  "-DANDROID_PLATFORM=android-24" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\mihai\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\mihai\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\mihai\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\mihai\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=S:\\Android projects\\GolfSimMobile\\OpenCV\\build\\intermediates\\cxx\\RelWithDebInfo\\6m5m1vg1\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=S:\\Android projects\\GolfSimMobile\\OpenCV\\build\\intermediates\\cxx\\RelWithDebInfo\\6m5m1vg1\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ^
+  "-BS:\\Android projects\\GolfSimMobile\\OpenCV\\.cxx\\RelWithDebInfo\\6m5m1vg1\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"

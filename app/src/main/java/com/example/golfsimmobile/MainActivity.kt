71efdfaf00a4151803d93f7017687053c54c6f6e
l-Spook-l <uaspookua@gmail.com>
2025-07-09 20:18:10 +0300

    feat: connect layouts to fragments and activity

--- app/src/main/java/com/example/golfsimmobile/MainActivity.kt
+++ app/src/main/java/com/example/golfsimmobile/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.golfsimmobile
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    // Объявляем кнопки
+    private lateinit var btnGameTab: Button
+    private lateinit var btnFindBallTab: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Устанавливаем XML-макет активности (main_menu.xml)
+        setContentView(R.layout.main_menu)
+
+        // Получаем ссылки на кнопки по ID
+        btnGameTab = findViewById(R.id.btnGameTab)
+        btnFindBallTab = findViewById(R.id.btnFindBallTab)
+
+        // При первом запуске сразу загружаем GameFragment в контейнер
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, GameFragment())
+            .commit()
+
+        // Обработчик нажатия на кнопку "Game"
+        btnGameTab.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, GameFragment())
+                .commit()
+        }
+
+        // Обработчик нажатия на кнопку "Find Ball"
+        btnFindBallTab.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, FindBallFragment())
+                .commit()
         }
     }
 }

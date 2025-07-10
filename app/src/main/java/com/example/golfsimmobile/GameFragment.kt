a00b4b89fc1955f827ddf5ecc23610cab8f72d7c
l-Spook-l <uaspookua@gmail.com>
2025-07-10 20:19:05 +0300

    feat: added requests and permissions for camera, video, and audio usage

--- app/src/main/java/com/example/golfsimmobile/GameFragment.kt
+++ app/src/main/java/com/example/golfsimmobile/GameFragment.kt
@@ -1,14 +1,39 @@
 package com.example.golfsimmobile
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.util.Size
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import org.opencv.android.OpenCVLoader
 
 class GameFragment : Fragment() {
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
+
+    private lateinit var textureView: TextureView
+    private lateinit var imageView: ImageView
+    private lateinit var handler: Handler
 
     private var isTracking = false
 
@@ -16,23 +41,63 @@ class GameFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.game, container, false)
+        return inflater.inflate(R.layout.game, container, false)
+    }
 
-        val trackButton = view.findViewById<Button>(R.id.trackBallButton)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val trackButton = view.findViewById<Button>(R.id.recordButton)  // изменить назв кнопки
+        imageView = view.findViewById(R.id.imageView)
+        textureView = view.findViewById(R.id.textureView)
+
+        val thread = HandlerThread("CameraThread")
+        thread.start()
+        handler = Handler(thread.looper)
+
+        // Инициализация обработчика разрешений
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            val allPermissionsGranted = permissions.values.all { it }
+            if (allPermissionsGranted) {
+                Toast.makeText(requireContext(), "Все разрешения получены!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Некоторые разрешения не были предоставлены.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        getPermissions()
 
         trackButton.setOnClickListener {
             isTracking = !isTracking
             if (isTracking) {
-                trackButton.text = "Остановить отслеживание"
+                trackButton.text = "Stop tracking ball"
+                trackButton.setBackgroundColor(Color.RED)
                 Toast.makeText(context, "Начинаем отслеживание мяча", Toast.LENGTH_SHORT).show()
                 // TODO: Запустить отслеживание
             } else {
-                trackButton.text = "Отслеживать мяч"
+                trackButton.text = "Start tracking ball"
+                trackButton.setBackgroundColor(Color.GREEN)
                 Toast.makeText(context, "Отслеживание остановлено", Toast.LENGTH_SHORT).show()
                 // TODO: Остановить отслеживание
             }
         }
+    }
 
-        return view
+    private fun getPermissions() {
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    android.Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ), 101
+            )
+        }
     }
 }

8dcfac789e5217983ed6d2a93dbae92bde072e00
l-Spook-l <uaspookua@gmail.com>
2025-07-10 20:22:33 +0300

    Remove generated files and folders from OpenCV from repository

--- OpenCV/build/intermediates/cxx/Debug/3m473z5i/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\mihai\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "S:\\Android projects\\GolfSimMobile\\OpenCV\\.cxx\\Debug\\3m473z5i\\x86_64" ^
-  opencv_jni_shared

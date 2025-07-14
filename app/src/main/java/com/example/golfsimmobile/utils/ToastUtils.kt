f26f39cd86f6d0eaaa03231b1651c04608b63a53
l-Spook-l <uaspookua@gmail.com>
2025-07-14 21:24:55 +0300

    feat: added utility function for showing Toast messages

---
+++ app/src/main/java/com/example/golfsimmobile/utils/ToastUtils.kt
@@ -0,0 +1,12 @@
+package com.example.golfsimmobile.utils  // ✅ важно
+
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+
+fun showToast(context: Context, message: String) {
+    Handler(Looper.getMainLooper()).post {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+}

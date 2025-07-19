09969d99cde3ce4c35b6ce3636e2b89e05b49ff6
l-Spook-l <uaspookua@gmail.com>
2025-07-19 17:15:01 +0300

    refactor: move gameFragment to class-level property in MainActivity

--- app/src/main/java/com/example/golfsimmobile/MainActivity.kt
+++ app/src/main/java/com/example/golfsimmobile/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     // Объявляем кнопки
     private lateinit var gameTabButton: Button
     private lateinit var findBallTabButton: Button
+    private lateinit var gameFragment: GameFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragment_container, gameFragment)
             .commit()
 
-        // Обработчик нажатия на кнопку "Game"
+        // Нажата "Игра" — скрыть кнопки поиска мяча
         gameTabButton.setOnClickListener {
             gameFragment.hideFindBallButtons()
         }
